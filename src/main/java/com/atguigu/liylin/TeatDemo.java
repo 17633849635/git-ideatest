971eb16d828dff36f895f203c5a0bf756b616549
liyilin <1798212408@qq.com>
2022-09-18 22:18:13 +0800

    创建项目

---
+++ src/main/java/com/atguigu/liylin/TeatDemo.java
@@ -0,0 +1,13 @@
+package com.atguigu.liylin;
+
+/**
+ * @program: git-ideatest
+ * @description
+ * @author: 李一林
+ * @create: 2022-09-18 22:17
+ **/
+public class TeatDemo {
+    public static void main(String[] args) {
+
+    }
+}
