194162d35d16115d4f03b09bc4e005764621baa5
liyilin <1798212408@qq.com>
2022-09-19 21:42:16 +0800

    升级项目 version third1

--- src/main/java/com/atguigu/liylin/TeatDemo.java
+++ src/main/java/com/atguigu/liylin/TeatDemo.java
@@ -10,5 +10,7 @@ public class TeatDemo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("two");
+
+        System.out.println(3333333);
     }
 }
